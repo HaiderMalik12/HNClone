aefbce989d766c0ef0da5e411bc9c712aa4c68a4
HaiderMalik <haidermalik504@gmail.com>
2017-09-14 20:26:09 +0500

    Set cron task to run after an hour

--- index.js
+++ index.js
@@ -54,7 +54,7 @@ if (!module.parent) {
     // listen on port config.port
     app.listen(config.port, () => {
         console.info(`server started on port ${config.port} (${config.env})`); // eslint-disable-line no-console
-        cron.schedule('*/60 * * * *', function () {
+        cron.schedule('0 * * * *', function () {
             saveArticles()
                 .then(articles => {
                     if (articles.length) {
