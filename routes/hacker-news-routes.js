f5646bd438c2234ac050ffe32dd48f86547c21d1
HaiderMalik <haidermalik504@gmail.com>
2017-09-15 16:19:40 +0500

    some cleanup

--- routes/hacker-news-routes.js
+++ routes/hacker-news-routes.js
@@ -6,8 +6,10 @@ const router = express.Router();
 router.route('/')
     .get(HackerNewsCtrl.getArticles);
 
+//Can't send the delete request from the FORM
+//Otherwise, I'll use delete method here
 router.route('/:id')
-    .post(HackerNewsCtrl.deleteArticle);
+    .get(HackerNewsCtrl.deleteArticle);
 
 
 module.exports = router;
